d27167771cadd57374d1c10f99a933df240267a0
Pipe2303 <piperoldanrestrepo@hotmail.com>
2024-03-03 21:37:25 -0500

    Register and Login

--- RegisterAndAuth_SisIntDis/Assets/Scripts/AuthController.cs
+++ RegisterAndAuth_SisIntDis/Assets/Scripts/AuthController.cs
@@ -1,18 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class AuthController : MonoBehaviour
 {
+    private string url = "https://sid-restapi.onrender.com";
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void sendRegister()
     {
+        AuthenticationData data = new AuthenticationData();
         
+        data.username = GameObject.Find("InputFieldUsername").GetComponent<TMP_InputField>().text;
+        data.password = GameObject.Find("InputFieldPassword").GetComponent<TMP_InputField>().text;
+
+        StartCoroutine("Register",JsonUtility.ToJson(data));
     }
+
+    public void sendLogin()
+    {
+        AuthenticationData data = new AuthenticationData();
+        
+        data.username = GameObject.Find("InputFieldUsername").GetComponent<TMP_InputField>().text;
+        data.password = GameObject.Find("InputFieldPassword").GetComponent<TMP_InputField>().text;
+
+        StartCoroutine("Login",JsonUtility.ToJson(data));
+    }
+
+    IEnumerator Register(string json)
+    {
+        UnityWebRequest request = UnityWebRequest.Put(url+"/api/usuarios", json);
+        request.method = "POST";
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+            if (request.responseCode == 200)
+            {
+                Debug.Log("Registro Exitoso");
+                StartCoroutine("Login", json);
+            }
+            else
+            {
+                Debug.Log(request.responseCode + "|" + request.error);
+            }
+        }
+    }
+    IEnumerator Login(string json)
+    {
+        UnityWebRequest request = UnityWebRequest.Put(url+"/api/auth/login", json);
+        request.method = "POST";
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+            if (request.responseCode == 200)
+            {
+                AuthenticationData data = JsonUtility.FromJson<AuthenticationData>(request.downloadHandler.text);
+                Debug.Log(data.token);
+            }
+            else
+            {
+                Debug.Log(request.responseCode + "|" + request.error);
+            }
+        }
+    }
+}
+[System.Serializable]
+public class AuthenticationData
+{
+    public string username;
+    public string password;
+    public UsuarioJson usuario;
+    public string token;
+}
+[System.Serializable]
+public class UsuarioJson
+{
+    public string _id;
+    public string username;
 }
